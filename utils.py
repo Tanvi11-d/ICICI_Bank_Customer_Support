c461945ac0aa361c97315ea14b880455ea1a5aef
Tanvi11-d <Tanvi.d@bitcodingsolutions.com>
2026-03-05 15:50:38 +0530

    feat:tool add

--- utils.py
+++ utils.py
@@ -108,13 +108,13 @@ async def icici_support(question):
             "transport": "http",
             
             "url": "https://innately-nondiocesan-daxton.ngrok-free.dev/mcp",
-            },
-            
-            "rag_tools": {
-                "transport": "http",
-                
-                "url": "https://innately-nondiocesan-daxton.ngrok-free.dev/mcp",
             }
+            
+            # "rag_tools": {
+            #     "transport": "http",
+                
+            #     "url": "https://innately-nondiocesan-daxton.ngrok-free.dev/mcp",
+            # }
         }
     )
     client_tools = await client.get_tools()
